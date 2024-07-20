349b17f3e0c6756a72c0f8298417aad4a6a17b22
wpanas <8766331+wpanas@users.noreply.github.com>
2024-07-20 18:39:45 +0200

    Add turboStripLowercaseAccents

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,29 @@
+import com.github.wpanas.turboStripLowercaseAccents
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.apache.commons.lang3.StringUtils.stripAccents
+import java.lang.Character.MAX_CODE_POINT
+import java.lang.Character.MIN_CODE_POINT
+
+class MainTest : StringSpec({
+    "should run" {
+        turboStripLowercaseAccents("ąć") shouldBe "ac"
+    }
+
+    "should be good for all from" {
+        assertSoftly {
+            IntRange(MIN_CODE_POINT, MAX_CODE_POINT)
+                .filter { Character.isLowerCase(it) }
+                .map { Character.toString(it) }
+                .forEach { input ->
+                    val turboStrippedString = turboStripLowercaseAccents(input)
+                    val casuallyStrippedString = stripAccents(input)
+                    withClue("\"$input\" to \"$casuallyStrippedString\",") {
+                        turboStrippedString shouldBe casuallyStrippedString
+                    }
+                }
+        }
+    }
+})
